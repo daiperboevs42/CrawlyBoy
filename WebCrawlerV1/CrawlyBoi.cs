a65131c57cdf61535a2268657477df8aa29d706a
Martin Emil Wøbbe <54886017+daiperboevs42@users.noreply.github.com>
2021-02-12 11:08:07 +0100

    initial commit

---
+++ WebCrawlerV1/CrawlyBoi.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text.RegularExpressions;
+
+namespace WebCrawlerV1
+{
+    public class CrawlyBoi
+    {
+        private Queue<Uri> frontier;
+        private Dictionary<string, bool> visitedUrls;
+
+        //public Dictionary<Uri, bool> allURLs;
+        public List<string> allURLs;
+        public void DoThingy()
+        {
+            Console.WriteLine("Please Write URL You Wish To Crawl");
+            UriBuilder ub = new UriBuilder(Console.ReadLine());
+            WebClient wc = new WebClient();
+            string webPage = wc.DownloadString(ub.Uri.ToString());
+            var hrefPattern = new Regex("href\\s*=\\s*(?:\"(?<1>[^\"]*)\"|(?<1>\\S+))", RegexOptions.IgnoreCase);
+
+            var urls = hrefPattern.Matches(webPage);
+            foreach (Match url in urls)
+            {
+                string newUrl = hrefPattern.Match(url.Value).Groups[1].Value;
+                Console.WriteLine(newUrl);
+                Uri myUri = new Uri(newUrl);
+
+                if(BasedAfURL(myUri, newUrl) != null)
+                {
+                    frontier.Enqueue(myUri);
+                    visitedUrls.Add(newUrl, true);
+                }
+                else
+                {
+                    visitedUrls.Add(newUrl, false);
+                }
+                
+            }
+        } 
+        
+        private Uri BasedAfURL(Uri baseUrl, string newUrl)
+        {
+            newUrl = newUrl.ToLower();
+            if (Uri.TryCreate(newUrl, UriKind.RelativeOrAbsolute, out var url))
+            {
+                return (Uri.TryCreate(baseUrl, url, out Uri absoluteUrl) ?
+               absoluteUrl : null);
+            }
+            return null;
+        }
+        
+        private void DoWhatITellYouToDo(List<string> allURLs)
+        {
+            foreach (var url in allURLs)
+            {
+                try
+                {
+                    WebRequest request = WebRequest.Create(url);
+                    request.Method = "GET";
+                    var meme = request.GetResponse();
+                    Console.WriteLine(meme); 
+                }
+                catch
+                {
+
+                }
+            }
+        }
+    }
+}
---
+++ WebCrawlerV1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Net;
+using System.Text.RegularExpressions;
+
+namespace WebCrawlerV1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CrawlyBoi cb = new CrawlyBoi();
+            cb.DoThingy();
+            
+        }
+    }
+}
